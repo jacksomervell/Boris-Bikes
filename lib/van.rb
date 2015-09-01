743788d292570cf3d2883f08a701430085fa7c46
Jack Somervell <jacksomervell@gmail.com>
2015-09-01 20:07:15 +0100

    completed basics of the van

--- lib/van.rb
+++ lib/van.rb
@@ -1,7 +1,28 @@
 class Van
 
-  def bike_count
-    0
+DEFAULT_CAPACITY = 15
+
+
+  def initialize options = {}
+    @bikes = []
+    @capacity = options.fetch(:capacity, DEFAULT_CAPACITY)  
   end 
+
+  def bike_count
+    @bikes.count
+  end 
+
+  def load bike
+  @bikes << bike
+  end
+
+  def unload bike
+    @bikes.delete bike 
+  end 
+
+  def full?
+    bike_count == @capacity
+  end 
+
 
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,10 +1,40 @@
 require_relative './spec_helper.rb'
 
+
 describe Van do
 
- it 'should be empty after we build it' do
+  def fill_van van
+    15.times {van.load(Bike.new)}
+  end 
+
+let (:van) {Van.new}
+let (:bike) {Bike.new}
+
+  it 'should be empty after we build it' do
     expect(van.bike_count).to eq 0
   end 
   
-  
+   it 'is able to load a bike' do
+    van.load(bike)
+    expect(van.bike_count).to eq 1
+  end
+
+  it 'is able to unload a bike' do
+    van.load(bike)
+    van.unload(bike)
+    expect(van.bike_count).to eq 0
+  end
+
+it 'should know when it has reached capacity' do
+    fill_van van
+    expect(van.full?).to be true    
+  end 
+
+  it 'should not accept bike if its full' do
+    fill_station station
+    expect {station.dock(bike)}.to raise_error RuntimeError 
+  end 
+
+
+
 end
